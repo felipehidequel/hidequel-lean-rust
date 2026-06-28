03316b95636253552f9bfb2a63228035698fc54d
Felipe Hidequel <92414576+felipehidequel@users.noreply.github.com>
2026-06-27 21:42:21 -0300

    Delete log-aggregator/target directory

--- log-aggregator/target/debug/deps/log_aggregator-929fd6cf48091bb2.d
+++
@@ -1,5 +0,0 @@
-/home/felipehidequel/hidequel-lean-rust/log-aggregator/target/debug/deps/log_aggregator-929fd6cf48091bb2.d: src/main.rs
-
-/home/felipehidequel/hidequel-lean-rust/log-aggregator/target/debug/deps/log_aggregator-929fd6cf48091bb2: src/main.rs
-
-src/main.rs:

0683d4fa2a52c147f095bc4b613a32c734f9064f
Felipe Hidequel <felipehidequel@gmail.com>
2026-06-27 21:41:14 -0300

    chore: add .gitignore and clean up target directory

--- log-aggregator/target/debug/deps/log_aggregator-f3a1916aff62843f.d
+++
@@ -1,5 +0,0 @@
-/home/felipehidequel/hidequel-lean-rust/log-aggregator/target/debug/deps/log_aggregator-f3a1916aff62843f.d: src/main.rs
-
-/home/felipehidequel/hidequel-lean-rust/log-aggregator/target/debug/deps/liblog_aggregator-f3a1916aff62843f.rmeta: src/main.rs
-
-src/main.rs:
--- log-aggregator/target/debug/log-aggregator.d
+++
@@ -1,1 +0,0 @@
-/home/felipehidequel/hidequel-lean-rust/log-aggregator/target/debug/log-aggregator: /home/felipehidequel/hidequel-lean-rust/log-aggregator/src/main.rs

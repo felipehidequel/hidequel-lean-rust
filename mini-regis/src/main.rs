5d330586b1d56e35c15343ecf5e984ada89a9f36
Felipe Hidequel <felipehidequel@gmail.com>
2026-07-04 15:09:18 -0300

    feat: interactive interface

--- mini-regis/src/main.rs
+++ mini-regis/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
-use std::time::{Instant, Duration};
+use std::io::{self, Write};
+use std::time::{Duration, Instant};
 
 struct ItemBanco {
     valor: String,
@@ -12,35 +13,34 @@ struct BancoKV {
 }
 
 impl BancoKV {
-
     fn new() -> Self {
         BancoKV {
             mapa: HashMap::new(),
         }
     }
 
-    fn set(&mut self, chave: String, valor: String, segundos_vida: Option<u64>){
+    fn set(&mut self, chave: String, valor: String, segundos_vida: Option<u64>) {
         let ttl = segundos_vida.map(Duration::from_secs);
-        
+
         let item = ItemBanco {
             valor,
             criado_em: Instant::now(),
             ttl,
         };
 
-        self.mapa.insert(chave,item);
+        self.mapa.insert(chave, item);
     }
 
     fn get(&mut self, chave: &str) -> Option<&String> {
         let mut expirou = false;
 
-        if let Some(item) = self.mapa.get(chave){
+        if let Some(item) = self.mapa.get(chave) {
             if let Some(tempo_limite) = item.ttl {
                 if item.criado_em.elapsed() > tempo_limite {
-                    expirou = true; 
+                    expirou = true;
                 }
             }
-       } else {
+        } else {
             return None;
         }
 
@@ -53,17 +53,57 @@ impl BancoKV {
     }
 }
 
+fn command_execute(banco: &mut BancoKV, linha: &str) {
+    let parts: Vec<&str> = linha.trim().split(' ').collect();
+
+    let command = match parts.get(0) {
+        Some(&cmd) => cmd.to_uppercase(),
+        None => return,
+    };
+
+    match command.as_str() {
+        "SET" => {
+            if let (Some(&chave), Some(&valor)) = (parts.get(1), parts.get(2)) {
+                let ttl = parts.get(3).and_then(|s| s.parse::<u64>().ok());
+
+                banco.set(String::from(chave), String::from(valor), ttl);
+                println!("Ok!");
+            } else {
+                println!("Erro: Uso correto -> SET <chave> <valor> [segundos]");
+            }
+        }
+        "GET" => {
+            if let Some(&chave) = parts.get(1) {
+                match banco.get(chave) {
+                    Some(v) => {
+                        println!("{v}");
+                    }
+                    None => {
+                        println!("(nil)")
+                    }
+                };
+            }
+        }
+        "EXIT" => {
+            println!("Saindo do mini-regis... Até mais!");
+            std::process::exit(0);
+        }
+        _ => println!("Erro: Comando desconhecido '{}'", command),
+    }
+}
+
 fn main() {
     let mut banco = BancoKV::new();
+    println!("=== MINI-REGIS INTERATIVO ===");
+    println!("Comandos aceitos: SET, GET, EXIT\n");
 
-    banco.set(String::from("session_token"), String::from("XC3234"), Some(2));
+    loop {
+        print!("mini-regis> ");
+        io::stdout().flush().unwrap(); // Força o texto acima a aparecer no terminal imediatamente
 
-    println!("Busca 1 (Imediata): {:?}", banco.get("session_token"));
+        let mut linha = String::new();
+        io::stdin().read_line(&mut linha).unwrap();
 
-    
-    println!("Dormindo por 3 segundos...");
-    std::thread::sleep(std::time::Duration::from_secs(3));
-
-    println!("Busca 2 (Após 3s): {:?}", banco.get("session_token"));
-    println!("Quantidade de itens na memória: {}", banco.mapa.len());
+        command_execute(&mut banco, &linha);
+    }
 }

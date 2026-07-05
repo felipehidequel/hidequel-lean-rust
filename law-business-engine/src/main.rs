7106449736db71d18a9ea08ae69e7994219e315f
Felipe Hidequel <felipehidequel@gmail.com>
2026-07-05 02:52:57 -0300

    feat: added custom var

--- law-business-engine/src/main.rs
+++ law-business-engine/src/main.rs
@@ -1,5 +1,11 @@
+use std::collections::HashMap;
+
+
+
 #[derive(Debug)]
 enum Expressao {
+    Variavel(String),
+
     // pure boolean value
     Constante(bool),
 
@@ -14,26 +20,39 @@ enum Expressao {
 }
 
 impl Expressao {
-    fn avaliar(&self) -> bool {
+    fn avaliar(&self, context: &HashMap<String, bool>) -> bool {
         match self {
             Expressao::Constante(val) => *val,
-            Expressao::Nao(expr_interna) => !expr_interna.avaliar(),
-            Expressao::Ou(esquerda, direita) => esquerda.avaliar() || direita.avaliar(),
-            Expressao::E(esquerda, direita) => esquerda.avaliar() && direita.avaliar(),
+            Expressao::Nao(expr_interna) => !expr_interna.avaliar(&context),
+            Expressao::Ou(esquerda, direita) => esquerda.avaliar(&context) || direita.avaliar(&context),
+            Expressao::E(esquerda, direita) => esquerda.avaliar(&context) && direita.avaliar(&context),
+
+            Expressao::Variavel(nome_var) => {
+                context.get(nome_var).copied().unwrap_or(false)
+            }
         }
     }
 }
 
 fn main() {
-    let regra = Expressao::E(
+    // Regra de negócio: O cliente ganha frete grátis se:
+    // (cupom_valido OU eh_vip) E (NAO frete_internacional)
+    let regra_frete_gratis = Expressao::E(
         Box::new(Expressao::Ou(
-            Box::new(Expressao::Constante(true)),
-            Box::new(Expressao::Constante(false)),
+            Box::new(Expressao::Variavel(String::from("cupom_valido"))),
+            Box::new(Expressao::Variavel(String::from("eh_vip"))),
         )),
         Box::new(Expressao::Nao(
-            Box::new(Expressao::Constante(false))
+            Box::new(Expressao::Variavel(String::from("frete_internacional")))
         )),
     );
 
-    println!("Resultado da regra: {}", regra.avaliar());
+    // Simulando os dados da requisição atual do cliente
+    let mut contexto_cliente = HashMap::new();
+    contexto_cliente.insert(String::from("cupom_valido"), false);
+    contexto_cliente.insert(String::from("eh_vip"), true);
+    contexto_cliente.insert(String::from("frete_internacional"), false);
+
+    let resultado = regra_frete_gratis.avaliar(&contexto_cliente);
+    println!("Cliente ganhou frete grátis? {}", resultado);
 }
